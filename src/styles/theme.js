6ac4aab4551a7a2fb8ad97d79c1352d9cb5b281c
Riley Miller <rmllr000@gmail.com>
2021-01-30 22:28:12 -0700

    feat: finalish colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -27,18 +27,25 @@ const theme = {
     // primary: "#f58634", // orange
     // primary: "#ec5858", // red
     // primary: "#09015f", // purple
-    primary: "#f37121", // another orange
+    // primary: "#f37121", // another orange
+    // primary: "#1553f0", // blue
+    // primary: "#09015f", // dark blue
+    primary: "#150485",
     // primary: "#000000",
     // secondary: "#098c8c",
     // secondary: "#007965", // green
     // secondary: "#09015f", // purple
-    secondary: `#1553f0`, // blue
+    // secondary: `#1553f0`, // blue
+    secondary: `#f37121`, // orange
+    // secondary: `#e25822`,
     // secondary: "#fd8c04",
     // accent: "#cca86e",
     // accent: "#00af91", // green
     // accent: "#ec5858", // red
     // accent: "#09015f", // purple
-    accent: "#1553f0", // blue
+    // accent: "#1553f0", // blue
+    accent: "#f37121", // orange
+    // accent: `#e25822`,
     // accent: `#FAB042`, // orange
     // accent: "#000000",
     logoAccent: `#FAB042`, // orange
